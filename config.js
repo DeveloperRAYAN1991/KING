c7466ff996f3dfabe5c7ceea5df2b107c78d7f6b
DeveloperRAYAN1991 <109234176+DeveloperRAYAN1991@users.noreply.github.com>
2022-07-13 18:58:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
         TOKEN: '', //bot token
         playing: 'Music Bot',
-        mongoDB: "", //mongoDB url.
-        dblToken: "", //top.gg token.
+        mongoDB: "971868615538270229", //mongoDB url.
+        dblToken: "OTgzMzA0MDI0NDQ1MDU1MDM2.GWKqrc.dwwrgUj8gdkmphXQ2eW91TGH8aQoPT7s47baik", //top.gg token.
  opt: {
         DJ: { 
                 commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
